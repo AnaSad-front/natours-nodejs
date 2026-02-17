28656d3e1c52a9769f2824a0559d231cc1254ee4
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-02-17 17:27:34 +0300

    implementing a global error handling middleware

--- app.js
+++ app.js
@@ -26,9 +26,24 @@ app.use("/api/v1/users", userRouter);
 
 // Handle all undefined routes (404 Not Found)
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  err.status = "fail";
+  next(err); // Pass the error to the global error handling middleware
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500; // Set default status code to 500 if not provided
+  err.status = err.status || "error"; // Set default status to 'error' if not provided
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
