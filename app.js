871d547665c25924a1255766e62cc50d0a17de67
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-03-02 20:33:37 +0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -33,6 +34,20 @@ app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  }),
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`)); // Middleware to serve static files from the 'public' directory
