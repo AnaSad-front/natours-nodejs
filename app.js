836c5a53eae69eaec1ea57f2d8f1f59f72f9a468
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-03-02 18:16:34 +0300

    setting security HTTP Headers (helmet package)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -10,10 +11,15 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet()); // Middleware to set security-related HTTP headers
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from the same API
 const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per windowMs
   windowMs: 60 * 60 * 1000, // 1 hour
@@ -21,9 +27,13 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter); // Apply the rate limiting middleware to all routes starting with /api
 
-app.use(express.json()); // Middleware to parse JSON bodies in requests
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`)); // Middleware to serve static files from the 'public' directory
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); // Add a custom property to the request object
   // console.log(req.headers);
