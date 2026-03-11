75245efe4cd5813453732171b3142ff97e6c5ff6
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-03-11 16:25:43 +0300

    first steps with pug

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get("/", (req, res) => {
-  res.status(200).render("base");
+  res.status(200).render("base", {
+    tour: "The Forest Hiker",
+    user: "Ana",
+  });
 });
 
 // Mount the routers on the app
