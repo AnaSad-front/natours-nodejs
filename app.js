c4b25af877729d89c71a678989ec3d7045093da4
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-03-11 17:33:04 +0300

    setting up the project structure

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 const { title } = require("process");
 
 const app = express();
@@ -67,27 +68,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    title: "Exciting tours for adventurous people",
-    tour: "The Forest Hiker",
-    user: "Ana",
-  });
-});
-
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All Tours",
-  });
-});
-
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker Tour",
-  });
-});
 
 // Mount the routers on the app
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
